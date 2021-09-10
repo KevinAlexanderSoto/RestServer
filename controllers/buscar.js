816559731c18b1b9283a5d7dc95b4e52138851b5
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-09-10 10:35:28 -0500

    implementado funcion buscar, busqueda potente  insensible

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -9,6 +9,7 @@ const coleccionesPermitidas = [ // para validar colecciones
 'roles'
 
 ];
+
 const buscarUsuarios = async (termino = '',res)=>{
     
     const ismongoid = await ObjectId.isValid(termino);
@@ -18,6 +19,63 @@ if (ismongoid) {
     return res.status(200).json({results : (usuario ) ? [usuario] : []});
 }
 
+const regex = new RegExp(termino,'i');//expresion regular para hacer el termino insensible a mayusculas y minusculas
+
+const usuarios = await Usuario.find({
+    $or :[ {nombre : regex }, {correo : regex}],
+    $and : [{ estado : true}] // una condicion u otra con mongo y and  
+});
+
+res.status(200).json({results : (usuarios ) ? [usuarios] : []});
+
+
+};
+
+const buscarCategorias = async (parametro='' , res)=>{
+
+    const ismongo = await ObjectId.isValid(parametro);
+
+    if (ismongo) {
+        cat = await Categoria.findById(parametro);
+        console.log(cat);
+        return res.status(200).json({results : (cat) ? [cat ] : []});
+
+    }
+
+    const exreg = new RegExp(parametro,'i');
+
+    const categorias = await Categoria.find({nombre : exreg , estado :true})
+    console.log(categorias);
+
+    res.status(200).json({
+        results : (categorias) ? [categorias] : []
+    });
+};
+
+const buscarProductos = async (parametro , res) =>{
+    let regexp = '';
+    const ismongo = await ObjectId.isValid(parametro);
+    if (ismongo) {
+        const producto = await (await Producto.findById(parametro)).populate('categoria','nombre');
+
+        return res.status(200).json({results : (producto) ? [producto] : []});
+    }
+
+    if (isNaN(parametro)) {
+       
+        regexp = new RegExp(parametro,'i');    
+    }else{
+        
+        regexp = parseInt(parametro,10);
+    }
+    ;
+    
+
+    const result = await Producto.find({
+        $or : [ {nombre : regexp},{descripcion : regexp}, {precio : (isNaN(regexp)) ? (-1) : regexp }],
+        $and :[{estado : true}]
+    }).populate('categoria','nombre');
+    res.status(200).json({results : (result) ? [result] : []});
 };
 
 const buscar=(req,res=response)=>{
@@ -35,9 +93,10 @@ const buscar=(req,res=response)=>{
         break
 
         case  'categorias':
+            buscarCategorias(termino,res);
         break;
         case 'productos':
-        
+            buscarProductos(termino,res);
         break;
     }
 
